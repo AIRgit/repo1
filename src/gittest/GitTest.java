c52585eccd9169ec71940a2e2461703c13d8f429
admin <admin@192.168.1.33>
2013-10-24 18:49:15 +0200

    int neco

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int neco = 5;
         // TODO code application logic here
     }
 }
